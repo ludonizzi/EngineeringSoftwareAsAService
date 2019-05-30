9876b15fa7c3b3aec5ebc7e8780304c546e0ed77
DanieleIacomini <danieleiacomini96@gmail.com>
2019-05-30 19:49:03 +0200

    adding front view

--- Project/config/routes.rb
+++ Project/config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'static_pages/home'
-  get 'static_pages/regolamento'
-  get 'static_pages/infoclan'
-  get 'static_pages/aboutapp'
-  get 'static_pages/contatti'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get 'regolamento'    => 'static_pages#regolamento'
+  get 'infoclan'    => 'static_pages#infoclan'
+  get 'aboutapp'    => 'static_pages#aboutapp'
+  get 'contatti'    => 'static_pages#contatti'
 end
