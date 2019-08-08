bde56de7740d9b6978008d3eb3ce6b02c3b6435a
DanieleIacomini <danieleiacomini96@gmail.com>
2019-08-08 11:54:15 +0200

    InfoClan admin

---
+++ Project/app/controllers/infoc_admin_controller.rb
@@ -0,0 +1,10 @@
+class InfocAdminController < ApplicationController
+  def infoclan_admin
+    @var = Integer(params[:id])
+    @users = User.all.order(created_at: :desc)
+    @users.each do |user|
+      user.vittorie = user.id
+      user.sconfitte = 5
+    end
+  end
+end
