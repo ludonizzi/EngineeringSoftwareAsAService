743fc4cdfe4a3a9950e705411603e5e1b000a8d1
Ingegneria Informatica e Automatica <cantisani.1707633@studenti.uniroma1.it>
2019-08-12 19:44:39 +0200

    Impostazioni Infoclan concluse

--- Project/app/controllers/infoc_admin_controller.rb
+++ Project/app/controllers/infoc_admin_controller.rb
@@ -2,6 +2,9 @@ class InfocAdminController < ApplicationController
   def infoclan_admin
     @var = Integer(params[:id])
     @users = User.all.order(created_at: :desc)
+    @clan1 = Clan.find(1)
+    @clan2 = Clan.find(2)
+    @clan3 = Clan.find(3)
     @users.each do |user|
       user.vittorie = user.id
       user.sconfitte = 5
--- Project/app/controllers/infoc_controller.rb
+++ Project/app/controllers/infoc_controller.rb
@@ -2,6 +2,11 @@ class InfocController < ApplicationController
   def infoclan
     id = params[:id]
     @user= current_user
+    @clan1 = Clan.find(1)
+    @clan2 = Clan.find(2)
+    @clan3 = Clan.find(3)
+
+
 
   end
 end
