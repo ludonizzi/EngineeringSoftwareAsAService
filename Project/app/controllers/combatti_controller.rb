7d9006d2a382be50dd2e83b15ed4e9babf81e82c
Damiano Ludovici <ludovici.damiano@gmail.com>
2019-08-13 18:53:48 +0200

    Pagina combatti conclusa

--- Project/app/controllers/combatti_controller.rb
+++ Project/app/controllers/combatti_controller.rb
@@ -7,7 +7,17 @@ class CombattiController < ApplicationController
     end
     def postcomb
 	@user = User.find(current_user.id)
-        @user2 = User.find(current_user.id+1)
+	@max = User.maximum("id")
+	@id = current_user.id
+	@id2 = (Random.rand(1..10))
+	
+        while (@id2 == 1 || @id2 > @max || @id2 == @id) do
+		@id2 = (Random.rand(1..10))
+		if (@id2 != 1 && @id2 <= @max && @id2 != @id && User.find(@id2).clan == User.find(current_user.id).clan)
+			@id2 = 1
+		end
+	end
+	@user2 = User.find(@id2)
     end
 
     def results
