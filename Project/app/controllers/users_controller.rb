641109620d6d9521420b31e9a852781c9f215d6b
Damiano Ludovici <ludovici.damiano@gmail.com>
2019-08-13 15:40:17 +0200

    Aggiunta funzione modifica equipaggiamento

---
+++ Project/app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+    	before_action :authenticate_user!
+	def update
+  		@user = User.find(params[:id])
+  		@user.update(att: params[:user][:att], def: params[:user][:def], arm: params[:user][:arm])
+  		redirect_to combatti_url(@user)
+	end
+end
