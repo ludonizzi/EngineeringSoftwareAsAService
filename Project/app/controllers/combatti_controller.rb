61a512e206e1ee80915de3e777f0daf7a5a25312
Damiano Ludovici <ludovici.damiano@gmail.com>
2019-08-14 03:42:11 +0200

    Fix di alcuni bug alla ricerca avversario

--- Project/app/controllers/combatti_controller.rb
+++ Project/app/controllers/combatti_controller.rb
@@ -9,20 +9,24 @@ class CombattiController < ApplicationController
 	@user = User.find(current_user.id)
 	@max = User.maximum("id")
 	@id = current_user.id
-	@id2 = (Random.rand(1..10))
+	@id2 = (Random.rand(1..20))
 	
-        while (@id2 == 1 || @id2 > @max || @id2 == @id) do
-		@id2 = (Random.rand(1..10))
-		if (@id2 != 1 && @id2 <= @max && @id2 != @id && User.find(@id2).clan == User.find(current_user.id).clan)
+        while (@id2 == 1 || @id2 > @max || @id2 == @id || User.exists?(id: @id2)==false) do
+		@id2 = (Random.rand(1..20))
+		if (User.exists?(id: @id2)==false)
+			@id2 = 1
+		elsif ((User.find(@id2).clan == User.find(current_user.id).clan) && (@id2 <= @max) && (@id2 != @id) && (@id2 != 1))
 			@id2 = 1
 		end
 	end
 	@user2 = User.find(@id2)
+	$user = @user
+	$user2 = @user2
     end
 
     def results
-	@u = User.find(current_user.id)
-        @u2 = User.find(current_user.id+1)
+	@u = $user
+        @u2 = $user2
 	@att = 0
 	@def = 0
 	@att2 = 0
