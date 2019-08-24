2a257573171e8c830b4febc76930accc2e3a0518
Ingegneria Informatica e Automatica <cantisani.1707633@studenti.uniroma1.it>
2019-08-24 22:00:49 +0200

    Bug SignUp risolto da testare

--- Project/app/controllers/application_controller.rb
+++ Project/app/controllers/application_controller.rb
@@ -25,9 +25,4 @@ class ApplicationController < ActionController::Base
         end
     end
 
-
-    rescue_from CanCan::AccessDenied do |exception|
-        redirect_to root_path, :alert => exception.message
-    end
-
 end
